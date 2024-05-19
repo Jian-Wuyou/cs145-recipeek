ed66d237e9aaf4078f00d557e6b94958fe874a9c
Jian-Wuyou <dramante@up.edu.ph>
2024-05-19 23:49:28 +0800

    feat: move logout button to popup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,11 @@
     import '../app.postcss';
     import { Toast, initializeStores, modeCurrent, setModeCurrent } from '@skeletonlabs/skeleton';
     import { onMount } from 'svelte';
+    import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
+    import { storePopup } from '@skeletonlabs/skeleton';
 
     initializeStores();
+    storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
 
     onMount(() => {
         setModeCurrent($modeCurrent);
--- src/routes/user/+layout.svelte
+++ src/routes/user/+layout.svelte
@@ -5,18 +5,37 @@
     import { UserCircle } from '@steeze-ui/heroicons';
     import { Icon } from '@steeze-ui/svelte-icon';
     import { session } from '$lib/store/session';
+    import { type PopupSettings, popup } from '@skeletonlabs/skeleton';
     function logout() {
         signOutUser().then(() => {
             goto('/');
         });
     }
+
+    const popupProfile: PopupSettings = {
+        event: 'click',
+        target: 'profile',
+        placement: 'bottom',
+    };
 </script>
 
 <div class="flex h-full flex-wrap">
     <div class="h-min basis-full">
         <NavBar>
-            <div class="flex items-center gap-2"><Icon class="w-8 stroke-2" src={UserCircle} />{$session.username}</div>
-            <button class="variant-filled btn" on:click={logout}> Logout </button>
+            <button class="btn flex items-center gap-2 hover:variant-soft-primary" use:popup={popupProfile}>
+                <span><Icon class="w-8 stroke-2" src={UserCircle} /></span>
+                <span>{$session.username}</span>
+            </button>
+            <div class="card w-48 p-3 shadow-xl" data-popup="profile">
+                <div class="bg-surface-100-800-token" />
+                <nav class="list-nav">
+                    <ul>
+                        <li>
+                            <button on:click={logout} class="variant-filled btn w-full text-center"> Logout </button>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </NavBar>
     </div>
     <div class="container mx-auto grow">
