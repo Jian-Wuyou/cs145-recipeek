2194325edf55bc298d93073ff1b59481265116d7
Jian-Wuyou <dramante@up.edu.ph>
2024-05-25 18:01:49 +0800

    feat: fetch pantry from DB

--- src/routes/user/pantry/+page.svelte
+++ src/routes/user/pantry/+page.svelte
@@ -8,6 +8,8 @@
         popup,
         type PopupSettings,
     } from '@skeletonlabs/skeleton';
+    import { db, auth } from '$lib/firebase/firebase.client';
+    import { ref, get } from 'firebase/database';
 
     // Setup filter chips
     let filters: string[] = [];
@@ -38,29 +40,45 @@
 
     let stock: { [name: string]: Output<typeof StockEntry> } = {};
 
-    // Create dummy stock data
-    let count = 20;
-    for (const [key, value] of Object.entries(ingredients)) {
-        stock[key] = {
-            category: value,
-            available: count,
-        };
-        count += 20;
-    }
+    const user = auth.currentUser;
+    const inventoryRef = ref(db, `users/${user?.uid}/inventory`);
 
-    // Change displayed data whenever filters change
-    let filtered = stock;
-
-    $: {
-        if (filters.length == 0) {
-            filtered = stock;
-        } else {
-            filtered = {};
-            for (const [key, value] of Object.entries(stock)) {
-                if (value.category.some(v => filters.includes(v))) filtered[key] = value;
+    const inventoryPromise = get(inventoryRef)
+        .then(snapshot => (snapshot.exists() ? snapshot.val() : {}))
+        .then((inventory: { [name: string]: number }) => {
+            stock = {};
+            for (const [k, v] of Object.entries(inventory)) {
+                stock[k] = {
+                    category: ingredients[k],
+                    available: v,
+                };
             }
-        }
-    }
+            return stock;
+        });
+
+    // // Create dummy stock data
+    // let count = 20;
+    // for (const [key, value] of Object.entries(ingredients)) {
+    //     stock[key] = {
+    //         category: value,
+    //         available: count,
+    //     };
+    //     count += 20;
+    // }
+
+    // // Change displayed data whenever filters change
+    // let filtered = stock;
+
+    // $: {
+    //     if (filters.length == 0) {
+    //         filtered = stock;
+    //     } else {
+    //         filtered = {};
+    //         for (const [key, value] of Object.entries(stock)) {
+    //             if (value.category.some(v => filters.includes(v))) filtered[key] = value;
+    //         }
+    //     }
+    // }
 </script>
 
 <div class="p-12">
@@ -93,17 +111,21 @@
                 </tr>
             </thead>
             <tbody>
-                {#each Object.entries(filtered) as [name, { category, available }]}
-                    <tr>
-                        <td>{name}</td>
-                        <td>
-                            {#each category as c}
-                                <span class="variant-filled chip mx-1">{c}</span>
-                            {/each}
-                        </td>
-                        <td>{available}</td>
-                    </tr>
-                {/each}
+                {#await inventoryPromise}
+                    Loading...
+                {:then inventory}
+                    {#each Object.entries(inventory) as [name, { category, available }]}
+                        <tr>
+                            <td>{name}</td>
+                            <td>
+                                {#each category as c}
+                                    <span class="variant-filled chip mx-1">{c}</span>
+                                {/each}
+                            </td>
+                            <td>{available}</td>
+                        </tr>
+                    {/each}
+                {/await}
             </tbody>
         </table>
     </div>
