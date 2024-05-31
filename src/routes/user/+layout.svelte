aa30288d909fc0c901011ef7205c7212b44ce44e
Jian-Wuyou <dramante@up.edu.ph>
2024-05-31 14:49:50 +0800

    feat: add jumbotron when no recipes available
    - fixes body not using full width on mobile
    - fixes uncentered body on mobile/edge

--- src/routes/user/+layout.svelte
+++ src/routes/user/+layout.svelte
@@ -26,23 +26,23 @@
     };
 </script>
 
-<div class="flex h-full flex-wrap">
+<div class="flex h-full flex-wrap justify-center">
     <div class="h-min basis-full">
         <NavBar>
             <svelte:fragment slot="lead">
                 <div class="flex auto-rows-fr">
-                    <a class="flex w-[50%] items-center gap-2 px-4 hover:variant-soft-primary" href="/user/recipes">
+                    <a class="hover:variant-soft-primary flex w-[50%] items-center gap-2 px-4" href="/user/recipes">
                         <span><Icon class="w-6 stroke-2" src={BookText} /></span>
                         <span>Recipes</span>
                     </a>
-                    <a class="flex w-[50%] items-center gap-2 px-4 hover:variant-soft-primary" href="/user/pantry">
+                    <a class="hover:variant-soft-primary flex w-[50%] items-center gap-2 px-4" href="/user/pantry">
                         <span><Icon class="w-6 stroke-2" src={ShoppingBag} /></span>
                         <span>Pantry</span>
                     </a>
                 </div>
             </svelte:fragment>
             <svelte:fragment slot="trail">
-                <button class="btn flex items-center gap-2 hover:variant-soft-primary" use:popup={popupProfile}>
+                <button class="btn hover:variant-soft-primary flex items-center gap-2" use:popup={popupProfile}>
                     <span><Icon class="w-6 stroke-2" src={UserCircle} /></span>
                     <span>{$sessionStore.username}</span>
                 </button>
@@ -84,7 +84,7 @@
             </svelte:fragment>
         </NavBar>
     </div>
-    <div class="container w-full grow p-12">
+    <div class="container w-full grow p-4 md:p-12">
         <slot />
     </div>
 </div>
--- src/routes/user/dashboard/+page.svelte
+++ src/routes/user/dashboard/+page.svelte
@@ -23,33 +23,69 @@
 </script>
 
 <div class="flex h-dvh w-full flex-col gap-5 p-12">
-    <div class=" w-full"><h1 class=" text-center text-5xl">Recipe of the Day</h1></div>
-    <div class=" w-full">
-        <div class="card flex overflow-hidden p-4 text-sm">
-            <div class="relative w-[30%] flex-none overflow-hidden">
-                <img alt={enough[pickedIndex]} src={pickedRecipe?.imgUrl} />
-            </div>
-            <div class="flex flex-col gap-4 px-5">
-                <div class="text-center text-3xl">
-                    {enough[pickedIndex]}
+    {#if enough.length > 0}
+        <div class=" w-full"><h1 class=" text-center text-5xl">Recipe of the Day</h1></div>
+        <div class=" w-full">
+            <div class="card flex overflow-hidden p-4 text-sm">
+                <div class="relative w-[30%] flex-none overflow-hidden">
+                    <img alt={enough[pickedIndex]} src={pickedRecipe?.imgUrl} />
                 </div>
-                <div>
-                    {pickedRecipe?.ingredients}
-                </div>
-                <hr />
-                <div>
-                    <p class="text-xl">Instructions</p>
-                    <ol class="list">
-                        {#if pickedRecipe}
-                            {#each Object.entries(pickedRecipe.instructions) as [id, instruction]}
-                                <li>
-                                    <span>{Number(id) + 1}. {instruction}</span>
-                                </li>
-                            {/each}
-                        {/if}
-                    </ol>
+                <div class="flex flex-col gap-4 px-5">
+                    <div class="text-center text-3xl">
+                        {enough[pickedIndex]}
+                    </div>
+                    <div>
+                        {pickedRecipe?.ingredients}
+                    </div>
+                    <hr />
+                    <div>
+                        <p class="text-xl">Instructions</p>
+                        <ol class="list">
+                            {#if pickedRecipe}
+                                {#each Object.entries(pickedRecipe.instructions) as [id, instruction]}
+                                    <li>
+                                        <span>{Number(id) + 1}. {instruction}</span>
+                                    </li>
+                                {/each}
+                            {/if}
+                        </ol>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    {:else}
+        <div class="mx-auto max-w-screen-xl px-4 py-8 text-center lg:py-16">
+            <h1
+                class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white md:text-5xl lg:text-6xl"
+            >
+                The pantry is nearly empty!
+            </h1>
+            <p class="mb-8 text-lg font-normal text-gray-500 dark:text-gray-400 sm:px-16 lg:px-48 lg:text-xl">
+                To get the best recipe suggestions, add more ingredients!
+            </p>
+            <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+                <a
+                    href="/user/pantry"
+                    class="inline-flex items-center justify-center rounded-lg bg-blue-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+                >
+                    Add ingredients
+                    <svg
+                        class="ms-2 h-3.5 w-3.5 rtl:rotate-180"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 14 10"
+                    >
+                        <path
+                            stroke="currentColor"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M1 5h12m0 0L9 1m4 4L9 9"
+                        />
+                    </svg>
+                </a>
+            </div>
+        </div>
+    {/if}
 </div>
